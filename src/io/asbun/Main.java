69b4b959214a5dff85f4755d6b473f1644d0e505
guidoasbun <gdrodrigo8@gmail.com>
2020-11-10 22:34:09 -0800

    basic method

--- src/io/asbun/Main.java
+++ src/io/asbun/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        calculateScore("Rodrigo", 500);
+    }
+
+    public static int calculateScore(String playerName, int score){
+        System.out.println("Player " + playerName + " scored " + score + " points ");
+        return score * 1000;
     }
 }
