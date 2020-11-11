f2b91d5e02cd03b62a835a782fa3c2e3314f5ba1
guidoasbun <gdrodrigo8@gmail.com>
2020-11-10 22:40:25 -0800

    basic overloaded method

--- src/io/asbun/Main.java
+++ src/io/asbun/Main.java
@@ -3,12 +3,19 @@ package io.asbun;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        calculateScore("Rodrigo", 500);
+
+        int newScore = calculateScore("Rodrigo", 500);
+        System.out.println("New score is " + newScore);
+        calculateScore(75);
     }
 
     public static int calculateScore(String playerName, int score){
         System.out.println("Player " + playerName + " scored " + score + " points ");
+        return score * 1000;
+    }
+
+    public static int calculateScore(int score){
+        System.out.println("Unnamed player scored " + " scored " + score + " points ");
         return score * 1000;
     }
 }
